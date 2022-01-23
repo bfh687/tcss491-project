cf32a39dda33e791d11e5c8843f34440c3794fb2
BenjuhminStewart <stewaben@uw.edu>
2022-01-22 19:30:26 -0800

    item drops

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ ASSET_MANAGER.queueDownload("./sprites/skeleton.png");
 // cursor downloads
 ASSET_MANAGER.queueDownload("./sprites/cursor.png");
 
+// misc asset downloads
+ASSET_MANAGER.queueDownload("./sprites/items.png");
+
 // tileset downloads
 ASSET_MANAGER.queueDownload("./sprites/map/grass_tileset.png");
 ASSET_MANAGER.queueDownload("./sprites/map/ground_tileset.png");
@@ -24,19 +27,37 @@ ASSET_MANAGER.queueDownload("./sprites/map/props.png");
 ASSET_MANAGER.queueDownload("./sprites/map/structures.png");
 
 ASSET_MANAGER.downloadAll(() => {
-    const canvas = document.getElementById("gameWorld");
-    const ctx = canvas.getContext("2d");
-    ctx.imageSmoothingEnabled = false;
+  const canvas = document.getElementById("gameWorld");
+  const ctx = canvas.getContext("2d");
+  ctx.imageSmoothingEnabled = false;
 
-    // add entities here, order matters for priority atm
-    const knight = new Knight(engine, ctx.canvas.width / 2, ctx.canvas.height / 2);
-    engine.addEntity(new HUD(engine, knight));
-    engine.addEntity(new Cursor(engine));
-    engine.addEntity(new Skeleton(engine, ctx.canvas.width / 2 + 40, ctx.canvas.height / 2));
-    engine.addEntity(knight);
-    engine.addEntity(new Map(engine));
+  // add entities here, order matters for priority atm
+  const knight = new Knight(
+    engine,
+    ctx.canvas.width / 2,
+    ctx.canvas.height / 2
+  );
+  //engine.addEntity(new HUD(engine, knight));
+  engine.addEntity(new Cursor(engine));
+  // engine.addEntity(
+  //   new Skeleton(engine, ctx.canvas.width / 2 + 40, ctx.canvas.height / 2)
+  // );
+  // engine.addEntity(
+  //   new Skeleton(engine, ctx.canvas.width / 2 + 80, ctx.canvas.height / 2)
+  // );
+  // engine.addEntity(
+  //   new Skeleton(engine, ctx.canvas.width / 2 + 120, ctx.canvas.height / 2)
+  // );
+  engine.addEntity(
+    new Skeleton(engine, ctx.canvas.width / 2 + 160, ctx.canvas.height / 2)
+  );
+  engine.addEntity(knight);
+  //engine.addEntity(new Map(engine));
+  // engine.addEntity(
+  //   new Item(engine, ctx.canvas.width / 2, ctx.canvas.height / 2)
+  // );
 
-    // initialize and start engine
-    engine.init(ctx);
-    engine.start();
+  // initialize and start engine
+  engine.init(ctx);
+  engine.start();
 });

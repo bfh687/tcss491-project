e4cc4bb640c8377c0a03473bc4bdcadda05f122a
BenjuhminStewart <stewaben@uw.edu>
2022-02-19 10:44:09 -0800

    change mob cluster to circle bound spawning

--- entities/enemies/mobcluster.js
+++ entities/enemies/mobcluster.js
@@ -11,41 +11,34 @@ class MobCluster {
   }
 
   makeCluster(x, y, mobs, type) {
+    let radius = Math.sqrt(216) * mobs;
     if (type == "skeleton") {
       for (let i = 0; i < mobs; i++) {
-        this.game.addEntity(
-          new Skeleton(
-            this.game,
-            x + i * this.randomNumber(this.minOffset, this.maxOffset),
-            y + i * this.randomNumber(this.minOffset, this.maxOffset)
-          )
-        );
+        let r1 = radius * Math.sqrt(this.randomNumber(0, 2));
+        let theta = Math.random() * 2 * Math.PI;
+        let xRand = x + r1 * Math.cos(theta);
+        let yRand = y + r1 * Math.sin(theta);
+        this.game.addEntity(new Skeleton(this.game, xRand, yRand));
       }
     } else if (type == "eyeball") {
       for (let i = 0; i < mobs; i++) {
-        this.game.addEntity(
-          new Eyeball(this.game, x + i * this.randomNumber(this.minOffset, this.maxOffset), y + i * this.randomNumber(this.minOffset, this.maxOffset))
-        );
+        let r1 = radius * Math.sqrt(this.randomNumber(0, 2));
+        let theta = Math.random() * 2 * Math.PI;
+        let xRand = x + r1 * Math.cos(theta);
+        let yRand = y + r1 * Math.sin(theta);
+        this.game.addEntity(new Eyeball(this.game, xRand - 64, yRand));
       }
     } else {
       for (let i = 0; i < mobs; i++) {
         const rand = this.randomNumber(0, 12);
+        let r1 = radius * Math.random();
+        let theta = Math.random() * 2 * Math.PI;
+        let xRand = x + r1 * Math.cos(theta);
+        let yRand = y + r1 * Math.sin(theta);
         if (rand <= 7) {
-          this.game.addEntity(
-            new Skeleton(
-              this.game,
-              x + i * this.randomNumber(this.minOffset, this.maxOffset),
-              y + i * this.randomNumber(this.minOffset, this.maxOffset)
-            )
-          );
+          this.game.addEntity(new Skeleton(this.game, xRand, yRand));
         } else {
-          this.game.addEntity(
-            new Eyeball(
-              this.game,
-              x + i * this.randomNumber(this.minOffset, this.maxOffset),
-              y + i * this.randomNumber(this.minOffset, this.maxOffset)
-            )
-          );
+          this.game.addEntity(new Eyeball(this.game, xRand - 64, yRand));
         }
       }
     }

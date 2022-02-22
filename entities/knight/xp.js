33a17d86b7594cd6f29354218f214b1dec577395
bfh687 <57474145+bfh687@users.noreply.github.com>
2022-02-21 19:25:13 -0800

    fix broken game

--- entities/knight/xp.js
+++ entities/knight/xp.js
@@ -1,6 +1,6 @@
 class XP {
   constructor(game) {
-    this.knight = this.game.knight;
+    this.knight = game.knight;
     this.baseXP = 1000;
     this.currLevel = 1;
     this.currXP = 0;
@@ -19,7 +19,7 @@ class XP {
   level() {
     this.currLevel++;
     this.skillPoints++;
-    this.xpNeeded = Math.pow(1.1, this.currLevel - 1) * this.baseXP;
+    this.xpNeeded = Math.pow(1.4, this.currLevel - 1) * this.baseXP;
 
     // testing hp scaling by level
     this.knight.levelUp();

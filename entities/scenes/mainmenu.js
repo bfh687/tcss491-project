69e9c031a1f317d0df23f9096a8b6fb1812ffd22
blake <bfh687@uw.edu>
2022-03-01 19:16:01 -0800

    unfinished menu screen

--- entities/scenes/mainmenu.js
+++ entities/scenes/mainmenu.js
@@ -8,12 +8,19 @@ class MainMenu {
 
     this.x = this.startX;
     this.y = this.startY;
-    this.alpha = 0;
     this.speed = 120;
+
+    // 0: title screen, 1: main menu, 2: controls, 3: credits
+    this.state = 0;
+
+    // main menu variables
+    this.alpha = 0;
+
+    // credits variables
+    this.creditsOffset = 0;
   }
 
   update() {
-    this.alpha += 2 * this.game.clockTick;
     this.x += this.game.clockTick * this.speed;
     this.y += (this.game.clockTick * this.speed) / 12;
 
@@ -25,15 +32,30 @@ class MainMenu {
       this.y = this.startY;
     }
 
-    if (this.game.keys[" "] && !this.game.camera.transition) this.game.camera.transition = new FadeTransition(this.game, 2.5, 1, false);
-    if (this.game.keys["Tab"]) this.game.camera.loadCredits(this.x, this.y);
+    if (this.state == 0) this.updateTitleScreen();
+    if (this.state == 1) this.updateMainMenu();
+    if (this.state == 2) this.updateControls();
+    if (this.state == 3) this.updateCredits();
+
+    if (this.game.keys[" "] && this.state == 0) this.state++;
   }
 
   draw(ctx) {
     // draw the map
     ctx.drawImage(this.spritesheet, 0, 0, 3216, 1760, -this.x, -this.y, 3216 * 2, 1760 * 2);
 
-    // draw the name of game
+    // draw screen relevant to state
+    if (this.state == 0) this.drawTitleScreen(ctx);
+    if (this.state == 1) this.drawMainMenu(ctx);
+    if (this.state == 2) this.drawControls(ctx);
+    if (this.state == 3) this.drawCredits(ctx);
+  }
+
+  updateTitleScreen() {
+    this.alpha += 2 * this.game.clockTick;
+  }
+
+  drawTitleScreen(ctx) {
     ctx.save();
     ctx.font = "128px bitpap";
 
@@ -57,4 +79,71 @@ class MainMenu {
     ctx.fillText("PRESS SPACE TO CONTINUE", this.game.width() / 2 - width, this.game.height() / 2 + 40);
     ctx.restore();
   }
+
+  updateMainMenu() {
+    const ctx = this.game.ctx;
+    const mouseBox = new BoundingBox(this.game.mouse.x, this.game.mouse.y, 1, 1);
+
+    const playWidth = ctx.measureText("PLAY").width;
+    this.playBox = new BoundingBox(this.game.width() / 2 - playWidth / 2, this.game.height() / 6 + 9 + 80 + 120, playWidth + 100, 130);
+
+    //if (mouseBox.collide(playBox) && this.game.left_click) this.state++;
+    const controlsBox = new BoundingBox();
+    const creditsBox = new BoundingBox();
+  }
+
+  drawMainMenu(ctx) {
+    drawBoundingBox(this.playBox, ctx, this.game, "purple");
+
+    // draw play
+    ctx.save();
+    ctx.font = "36px bitpap";
+
+    var title = "PLAY";
+    var width = ctx.measureText(title).width / 2;
+    ctx.fillStyle = "black";
+    ctx.globalAlpha = 0.4;
+    ctx.fillText(title, this.game.width() / 2 - width + 3, this.game.height() / 6 + 9 + 80 + 120 + 3);
+
+    ctx.globalAlpha = 1;
+    ctx.fillStyle = "white";
+    ctx.fillText(title, this.game.width() / 2 - width, this.game.height() / 6 + 9 + 80 + 120);
+    ctx.restore();
+
+    // draw controls
+    ctx.save();
+    ctx.font = "36px bitpap";
+
+    title = "CONTROLS";
+    width = ctx.measureText(title).width / 2;
+    ctx.fillStyle = "black";
+    ctx.globalAlpha = 0.4;
+    ctx.fillText(title, this.game.width() / 2 - width + 3, this.game.height() / 6 + 9 + 80 + 120 + 40 + 3);
+
+    ctx.globalAlpha = 1;
+    ctx.fillStyle = "white";
+    ctx.fillText(title, this.game.width() / 2 - width, this.game.height() / 6 + 9 + 80 + 120 + 40);
+    ctx.restore();
+
+    // draw controls
+    ctx.save();
+    ctx.font = "36px bitpap";
+
+    title = "CREDITS";
+    width = ctx.measureText(title).width / 2;
+    ctx.fillStyle = "black";
+    ctx.globalAlpha = 0.4;
+    ctx.fillText(title, this.game.width() / 2 - width + 3, this.game.height() / 6 + 9 + 80 + 120 + 80 + 3);
+
+    ctx.globalAlpha = 1;
+    ctx.fillStyle = "white";
+    ctx.fillText(title, this.game.width() / 2 - width, this.game.height() / 6 + 9 + 80 + 120 + 80);
+    ctx.restore();
+  }
+
+  updateCredits() {}
+  drawCredits(ctx) {}
+
+  updateControls() {}
+  drawControls(ctx) {}
 }

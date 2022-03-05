45898b12fa9bbbf75e16db6a372495e7bbc8fe37
Benjamin Stewart <82689821+BenjuhminStewart@users.noreply.github.com>
2022-03-04 18:43:50 -0800

    stop timer when player dies

--- engine/timer.js
+++ engine/timer.js
@@ -1,17 +1,19 @@
 class Timer {
-    constructor() {
-        this.gameTime = 0;
-        this.maxStep = 0.05;
-        this.lastTimestamp = 0;
-    };
+  constructor() {
+    this.gameTime = 0;
+    this.maxStep = 0.05;
+    this.lastTimestamp = 0;
+  }
 
-    tick() {
-        const current = Date.now();
-        const delta = (current - this.lastTimestamp) / 1000;
-        this.lastTimestamp = current;
+  tick() {
+    const current = Date.now();
+    const delta = (current - this.lastTimestamp) / 1000;
+    this.lastTimestamp = current;
 
-        const gameDelta = Math.min(delta, this.maxStep);
-        this.gameTime += gameDelta;
-        return gameDelta;
-    };
-};
+    const gameDelta = Math.min(delta, this.maxStep);
+    if (engine.knight && engine.knight.state != 4) {
+      this.gameTime += gameDelta;
+    }
+    return gameDelta;
+  }
+}
